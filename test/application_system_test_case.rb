2ae527ae3907b44063e72506908ac1040cdddd66
gisa chris <gisachris24@gmail.com>
2023-09-21 12:00:11 -0200

    Fix Linter Errors In System Test Case File

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 WINDOWS_HOST = `cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'`.strip
-CHROMEDRIVER_URL = "http://#{WINDOWS_HOST}:9515/"
+CHROMEDRIVER_URL = "http://#{WINDOWS_HOST}:9515/".freeze
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  #driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
   driven_by :selenium_remote_chrome
 
